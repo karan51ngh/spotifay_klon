752ab4c549798e5e03b4a3565a5887288b79eafe
karan51ngh <karansingh9535@gmail.com>
2023-03-20 14:18:01 +0530

    progressBar updation

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 console.log("Spotifay Nachinayetsya");
 
 // variables init
+let songIndex = 0;
 let audioElement = new Audio('1.mp3');
 let masterPLay = document.getElementById('masterPLay');
-let myProgressBar = document.getElementById('myProgressBar')
+let myProgressBar = document.getElementById('myProgressBar');
+let playingGif = document.getElementById("playingGif");
 
 let songs = [
     {songName: "NEFFEX", filePath:"songs/1.mp3", coverPath: "covers/1.jpg"},
@@ -23,16 +25,25 @@ masterPLay.addEventListener('click', () => {
         // changing Play button to Pause Button
         masterPLay.classList.remove('fa-circle-play');
         masterPLay.classList.add('fa-circle-pause');
+        playingGif.style.opacity = 1;
     }
     else{
         audioElement.pause();
         masterPLay.classList.remove('fa-circle-pause');
         masterPLay.classList.add('fa-circle-play');
+        playingGif.style.opacity = 0;
     }
 })
 
 // Listening to Events
-myProgressBar.addEventListener('timeupdate', () =>{
-    console.log("time update event listener")
-    // update seekbar
+audioElement.addEventListener('timeupdate', () =>{
+    // console.log("time update event listener")
+    // update progressbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100); // percentage of the progress of the song
+    myProgressBar.value = progress;
+})
+
+// update on clicking the progress bar
+myProgressBar.addEventListener('change', ()=> {
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
 })
